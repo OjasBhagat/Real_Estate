e7b681a082e15b5d9d8c60cbb43ef95bb3e578de
OjasBhagat <ojasbhagat8304@gmail.com>
2025-05-27 17:34:39 +0530

    Initial commit

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            At LuxeLiving, we specialize in helping you find more than just a property – we help you find your perfect place to call home. Whether you're buying, selling, renting, or investing, our experienced team is here to guide you every step of the way. With deep local market knowledge, personalized service, and a commitment to excellence, we make real estate simple, transparent, and rewarding.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
